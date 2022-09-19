035d26d24e4cf8f06def46c52abd9ac6ffde445d
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-09-19 11:07:14 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    player.change(LedSpriteProperty.Y, 1)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    player.move(-1)
+})
+input.onButtonPressed(Button.B, function () {
+    player.change(LedSpriteProperty.Y, -1)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    player.move(1)
+})
+let player: game.LedSprite = null
+basic.showIcon(IconNames.Angry)
+let enemy = game.createSprite(randint(0, 5), randint(0, 5))
+player = game.createSprite(0, 0)
 basic.forever(function () {
 	
 })
